3c06774eb5b2a0706d41d35c2422b2f4747ccbae
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2024-02-13 11:37:04 +0500

    working on deployment on render

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -16,12 +17,27 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("CHAT APP");
+  res.send("CHAT APP is running successfully");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+//  Deployment
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "forntend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("CHAT APP is running successfully");
+  });
+}
+
+//  Deployment
 
 app.use(notFound);
 app.use(errorHandler);
